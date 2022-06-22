1c4dac151eccb698b1ff61e8943220d5669484b1
Romain Rouphael <romainrouphael@gmail.com>
2022-06-22 18:18:35 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import launcher
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-# Launch the desired bot with the parameters defined in the configuration.yml file
-bot = launcher.bot()
-logging.info(bot)
